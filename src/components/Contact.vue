f5c76270caba7aff355d81b02d7d34e8cd66a74e
Abdussalam <abdussalamabdulmojeed@gmail.com>
2025-05-27 10:12:53 +0100

    want to check images folder

---
+++ src/components/Contact.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class="contact">
+   <h2>Contact Us</h2>
+    <p class="">Most calendars are designed for teams. <span class="d-none d-lg-block">Slate is designed for freelancers</span></p>
+    <section class="details">
+        <div class="form">
+            <h3>Contact Us</h3>
+            <form action="" method="post">
+                <input type="text" name="fname" id="fname" placeholder="Your Name">
+                <input type="email" name="email" id="email" placeholder="Your Email">
+                <textarea name="message" id="" cols="30" rows="10" placeholder="Your Message"></textarea>
+                <input
+                    name=""
+                    id=""
+                    class="btn btn-primary"
+                    type="button"
+                    value="Send"
+                />
+                
+            </form>
+        </div>
+        <div class="location">
+            <div class="mail">
+                <p><img src="/src/assets/logo.svg" alt=""><span>6386 Spring St Undefined Anchorage, Georgia 12473 United States</span></p>
+            </div>
+        </div>
+    </section>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style  scoped>
+.form{
+    width: 40%;
+    border: 2px solid red;
+    text-align: center;
+}
+form{
+    width: 90%;
+    border: 2px solid red;
+    margin: 10px auto;
+    display: flex;
+    flex-direction: column;
+    gap: 30px;
+}
+input[type='text'], input[type='email']{
+    width: 70%;
+    margin: 0 auto;
+    padding: 10px;
+    border: 0;
+    border-radius: 20px;
+    background-color: #ccc;
+}
+textarea{
+    width: 70%;
+    border: 0;
+    margin: 0 auto;
+     border-radius: 20px;
+    background-color: #ccc;
+}input[type='button']{
+width: 27%;
+cursor: pointer;
+margin: 0 66px 0;
+border-radius: 20px;
+}
+</style>
---
+++ src/components/PricingTag.vue
@@ -0,0 +1,144 @@
+<template>
+  <div class="pricing">
+    <h2>Pricing</h2>
+    <p class="">Most calendars are designed for teams. <span class="d-none d-lg-block">Slate is designed for freelancers</span></p>
+
+    <section class="price-card ">
+      <div class="card cols">
+        <h2>FREE</h2>
+        <p>Organize across all apps by hand</p>
+        <div class="price d-flex">
+          <h2>0 <span>$</span></h2>
+          <p>Per Month</p>
+        </div>
+        <p>Pricing Feature</p>
+        <p>Pricing Feature</p>
+        <p>Pricing Feature</p>
+        <p>Pricing Feature</p>
+        <p>Pricing Feature</p>
+        <button class="hover">Order Now</button>
+      </div>
+      <div class="card">
+        <h2>STANDARD</h2>
+        <p>Organize across all apps by hand</p>
+        <div class="price d-flex">
+          <h2>10 <span>$</span></h2>
+          <p>Per Month</p>
+        </div>
+        <p>Pricing Feature</p>
+        <p>Pricing Feature</p>
+        <p>Pricing Feature</p>
+        <p>Pricing Feature</p>
+        <p>Pricing Feature</p>
+        <button class="hover">Order Now</button>
+      </div>
+      <div class="card">
+        <h2>BUSINESS</h2>
+        <p>Organize across all apps by hand</p>
+        <div class="price d-flex">
+          <h2>99 <span>$</span></h2>
+          <p>Per Month</p>
+        </div>
+        <p>Pricing Feature</p>
+        <p>Pricing Feature</p>
+        <p>Pricing Feature</p>
+        <p>Pricing Feature</p>
+        <p>Pricing Feature</p>
+        <button class="hover">Order Now</button>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'pricingTag',
+}
+</script>
+
+<style scoped>
+.pricing {
+  background-color: rgb(4, 4, 46);
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  color: #fff;
+  padding: 40px 0;
+  /* justify-items: center; */
+}
+.price-card{
+    display: flex;
+    width: 60%;
+    flex-direction: row;
+    justify-content: space-between;
+    margin: 20px auto;
+    /* border: 2px solid red; */
+}
+.card{
+    width: 30%;
+    background-color: #fff;
+    cursor: pointer;
+    transition: all 0.5s;
+}
+.card:hover{
+    background-color: rgb(14, 154, 219);
+    color: #fff;
+    padding: 20px;
+    margin: -10px;
+}
+button{
+    border: 0;
+    width: 60%;
+    padding: 10px 20px;
+    border-radius: 20px;
+    margin: 10px auto;
+    background-color: rgb(14, 154, 219);
+}
+.card:hover button{
+    background-color: #fff;
+    color: rgb(14, 154, 219);
+}
+.price{
+    /* display: flex; */
+    width: 75%;
+    margin: 10px auto;
+    /* border: 2px solid red; */
+    align-items: end;
+}
+.price h2{
+    font-size: 4rem;
+    font-weight: 900;
+    display: flex;
+    gap: 10px;
+}
+.price h2 > span{
+    font-size: 1.4rem;
+    font-weight: 900;
+    /* border: 2px solid red; */
+    margin-top: 10px;
+}
+.price > p{
+    margin-left: -20px;
+    /* border: 2px solid red; */
+}
+@media screen and (max-width:1024px){
+.price-card{
+    width: 97%;
+    gap: 10px;
+}
+.card{
+    width: 35%;
+}
+}
+@media screen and (max-width:568px) {
+    .price-card{
+        flex-direction: column;
+        gap: 30px;
+        width: 80%;
+    }
+    .card{
+        width: 100%;
+    }
+}
+</style>
