ce9c763056ef4bb2173f2d49f9bb47c137ca3d7f
saliu ridwan <ridwansaliu84@gmail.com>
2025-05-28 14:17:09 +0100

    comrade initial commit

---
+++ src/components/button.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+<button class="bg-primary btn text-light d-block">{{ textBtn }}</button>
+    </div>
+</template>
+
+<script>
+    export default {
+         props: [
+      'btnText'
+    ],
+    data(){
+        return{
+            textBtn: this.btnText
+        }
+    }
+    }
+</script>
+
+<style scoped>
+.btn{
+    border: none;
+    font-weight: 500;
+    margin: 30px auto !important;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,15 +1,21 @@
-import './assets/main.css'
+// import './assets/main.css'
 
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+// import { createPinia } from 'pinia'
 
 import App from './App.vue'
-import router from './router'
+// import router from './router'
 import 'bootstrap/dist/css/bootstrap.min.css' 
 import 'bootstrap' 
+import buttonView from './components/button.vue' 
+import testimonial from './components/testimonial.vue' 
+// import partnerViews from './views/partnerViews.vue'
 const app = createApp(App)
 
-app.use(createPinia())
-app.use(router)
+// app.use(createPinia())
+// app.use(router)
+app.component('button-view', buttonView)
+app.component('testimonial-view', testimonial)
+// app.component('partener-view', partnerViews)
 
 app.mount('#app')
